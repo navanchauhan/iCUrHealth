556158f82180d72a7f9585d21a62320de3cd5dc0
Navan Chauhan <navanchauhan@gmail.com>
2024-02-09 21:12:22 -0700

    Initial Commit

---
+++ iCUrHealth/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iCUrHealth
+//
+//  Created by Navan Chauhan on 2/9/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iCUrHealth/iCUrHealthApp.swift
@@ -0,0 +1,17 @@
+//
+//  iCUrHealthApp.swift
+//  iCUrHealth
+//
+//  Created by Navan Chauhan on 2/9/24.
+//
+
+import SwiftUI
+
+@main
+struct iCUrHealthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
